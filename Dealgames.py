6c6e765dd7d8e6352dd37f0ff7542fb1e23684d8
Mathias <kammermayermathias@gmail.com>
2024-08-27 23:43:16 +0200

    Added duction to direct site when youy click on buy button + progressbar added

--- Dealgames.py
+++ Dealgames.py
@@ -11,23 +11,25 @@ import threading
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
+from concurrent.futures import ThreadPoolExecutor
 
 
 def search_all_sites():
     game_name = search_bar.get()
     print(f"Searching for game: {game_name} ...")
     clear_search_results()  # Efface les résultats de recherche précédents
+    progress_bar['value'] = 0
+    progress_bar['maximum'] = 4  # Nombre de recherches (1 par site)
     threading.Thread(target=lambda: search_g2a(game_name)).start()
     threading.Thread(target=lambda: search_eneba(game_name)).start()
     threading.Thread(target=lambda: search_gamivo(game_name)).start()
     threading.Thread(target=lambda: search_indiegala(game_name)).start()
-
 def clear_search_results():
     # Détruit tous les widgets enfants de scrollable_frame
     for widget in scrollable_frame.winfo_children():
         widget.destroy()
 
-def add_search_result(site_name, game_title, price, image_url=None):
+def add_search_result(site_name, game_title, price, image_url=None,url=None):
     frame = ttk.Frame(scrollable_frame)
     frame.pack(fill=tk.X, padx=10, pady=5)
 
@@ -56,19 +58,24 @@ def add_search_result(site_name, game_title, price, image_url=None):
         image_label.grid(row=0, column=0, rowspan=2, padx=10, pady=10, sticky="nw")
 
     # Titre du jeu avec padding en haut
-    title_label = ttk.Label(frame, text=game_title, font=("Helvetica", 14), wraplength=450)
+    title_label = ttk.Label(frame, text=game_title, font=("Helvetica", 15), wraplength=450)
     title_label.grid(row=0, column=1, columnspan=2, sticky='nw', pady=(10, 0))  # Ajout de pady pour baisser le texte
 
     # Nom du site avec padding en haut
-    site_label = ttk.Label(frame, text=site_name, font=("Helvetica", 10, "bold"), foreground='orange')
-    site_label.grid(row=1, column=1, columnspan=2, sticky='nw', pady=(5, 0))  # Ajout de pady pour ajuster le texte
+    site_label = ttk.Label(frame, text=site_name, font=("Helvetica", 12, "bold"), foreground='orange')
+    site_label.grid(row=1, column=1, columnspan=2, sticky='nw', pady=(4, 0))  # Ajout de pady pour ajuster le texte
 
     # Créer un frame pour le bouton et le prix, pour les aligner à droite
     right_frame = ttk.Frame(frame)
     right_frame.grid(row=0, column=3, rowspan=2, sticky='e')  # Place le frame à droite, occupe deux lignes
+    
+    def open_url():
+        if url:
+            import webbrowser
+            webbrowser.open(url)
 
     # Bouton d'achat
-    buy_button = ttk.Button(right_frame, text="Acheter", bootstyle="primary")
+    buy_button = ttk.Button(right_frame, text="Acheter", bootstyle="primary",command=open_url)
     buy_button.pack(padx=10, pady=5, anchor='e')  # Aligner le bouton à droite
 
     # Étiquette de prix
@@ -82,7 +89,6 @@ def add_search_result(site_name, game_title, price, image_url=None):
 def search_g2a(game_name):
     encoded_game_name = urllib.parse.quote(game_name)
     options = Options()
-    # options.add_argument('--headless')  # Uncomment to run browser in headless mode
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     try:
@@ -94,7 +100,6 @@ def search_g2a(game_name):
 
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             all_games = soup.find_all('li', class_="sc-gIvpjk kgKIqe indexes__StyledProductBox-wklrsw-122 beGIwZ productCard")
-            print(f"[G2A]Total number of games found: {len(all_games)}")
 
             if not all_games:
                 print("[G2A] End of results.")
@@ -108,15 +113,20 @@ def search_g2a(game_name):
                 price_tag = game.find("span", class_="sc-iqAclL sc-crzoAE dJFpVb eqnGHx sc-bqGGPW gjCrxq")
                 if price_tag:
                     item['Price'] = price_tag.text.strip()
-                image_tag = game.find("img", src=True)  # Trouver l'image avec la balise <img>
+                image_tag = game.find("img", src=True)
                 if image_tag:
                     item['Image'] = image_tag['src']
-                if 'Title' in item:
-                    fen_main.after(0, lambda: add_search_result("G2A", item['Title'], item['Price'], item.get('Image')))
+                link_tag = game.find("a", href=True)  # Extraction de l'URL du jeu
+                if link_tag:
+                    item['URL'] = "https://www.g2a.com" + link_tag['href']  # Construire l'URL complète
+
+                if 'Title' in item and 'Price' in item and 'URL' in item:
+                    fen_main.after(0, lambda: add_search_result("G2A", item['Title'], item['Price'], item.get('Image'), item['URL']))
             page_number += 1
     except Exception as e:
         print(f"Error searching G2A: {e}")
     finally:
+        fen_main.after(0, lambda: progress_bar.step(1))  # Incrémente la barre de progression
         driver.quit()
 
 
@@ -152,12 +162,16 @@ def search_eneba(game_name):
                 image_tag = game.find("img", src=True)  # Trouver l'image avec la balise <img>
                 if image_tag:
                     item['Image'] = image_tag['src']
-                if 'Title' in item and 'Price' in item:
-                    fen_main.after(0, lambda: add_search_result("Eneba", item['Title'], item['Price'], item.get('Image')))
+                link_tag = game.find("a", href=True)  # Extraction de l'URL du jeu
+                if link_tag:
+                    item['URL'] = "https://www.eneba.com" + link_tag['href']  # Construire l'URL complète
+                if 'Title' in item and 'Price' in item and 'URL' in item:
+                    fen_main.after(0, lambda: add_search_result("Eneba", item['Title'], item['Price'], item.get('Image'), item['URL']))
             page_number += 1
     except Exception as e:
         print(f"Error searching Eneba: {e}")
     finally:
+        fen_main.after(0, lambda: progress_bar.step(1))  # Incrémente la barre de progression
         driver.quit()
 
 
@@ -197,46 +211,60 @@ def search_gamivo(game_name):
                 image_tag = game.find("img", src=True)  # Find the image with the <img> tag
                 if image_tag:
                     item['Image'] = image_tag['src']
-                if 'Title' in item and 'Price' in item:
-                    fen_main.after(0, lambda: add_search_result("Gamivo", item['Title'], item['Price'], item.get('Image')))
+                link_tag = game.find("a", href=True)  # Extraction de l'URL du jeu
+                if link_tag:
+                    item['URL'] = "https://www.gamivo.com" + link_tag['href']  # Construire l'URL complète
+                if 'Title' in item and 'Price' in item and 'URL' in item:
+                    fen_main.after(0, lambda: add_search_result("Gamivo", item['Title'], item['Price'], item.get('Image'), item['URL']))
     except Exception as e:
         print(f"Error searching Gamivo: {e}")
     finally:
+        fen_main.after(0, lambda: progress_bar.step(1))  # Incrémente la barre de progression
         driver.quit()
 
 def search_indiegala(game_name):
     encoded_game_name = urllib.parse.quote(game_name)
     options = Options()
-    # options.add_argument('--headless')  # Uncomment to run browser in headless mode
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    
-    
-    url = f"https://www.indiegala.com/search/{encoded_game_name}/store-games"
-    driver.get(url)
-    time.sleep(1)  # Adjust the wait time if necessary
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
 
-    all_games = soup.find_all('div', class_="relative main-list-results-item")
-    print(f"[Indiegala]Total number of games found: {len(all_games)}")
+    try:
+        page_number = 1
+        while True:
+            url = f"https://www.indiegala.com/store?search={encoded_game_name}&page={page_number}"
+            driver.get(url)
+            time.sleep(0.25)
 
-    for game in all_games:
-        item = {}
-        title_tag = game.find("h3", class_="bg-gradient-red")
-        if title_tag:
-            item['Title'] = title_tag.text.strip()
-        else:
-            print("Title not found for this element.")
-        price_tag = game.find("div", class_="main-list-results-item-price-new")
-        if price_tag:
-            item['Price'] = price_tag.text.strip()
-        else:
-            print("Price not found for this element.")
-        image_tag = game.find("img", src=True)  # Find the image with the <img> tag
-        if image_tag:
-            item['Image'] = image_tag['src']
-        if 'Title' in item and 'Price' in item:
-            fen_main.after(0, lambda: add_search_result("Indiegala", item['Title'], item['Price'], item.get('Image')))
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            all_games = soup.find_all('div', class_="product-tile game-tile")
+
+            if not all_games:
+                print("[Indiegala] End of results.")
+                break
+
+            for game in all_games:
+                item = {}
+                title_tag = game.find("h5", class_="ellipsis_title")
+                if title_tag:
+                    item['Title'] = title_tag.text.strip()
+                price_tag = game.find("div", class_="price-current")
+                if price_tag:
+                    item['Price'] = price_tag.text.strip()
+                image_tag = game.find("img", src=True)
+                if image_tag:
+                    item['Image'] = image_tag['src']
+                link_tag = game.find("a", href=True)  # Extraction de l'URL du jeu
+                if link_tag:
+                    item['URL'] = "https://www.indiegala.com" + link_tag['href']  # Construire l'URL complète
+
+                if 'Title' in item and 'Price' in item and 'URL' in item:
+                    fen_main.after(0, lambda: add_search_result("Indiegala", item['Title'], item['Price'], item.get('Image'), item['URL']))
+            page_number += 1
+    except Exception as e:
+        print(f"Error searching Indiegala: {e}")
+    finally:
+        driver.quit()
+        fen_main.after(0, lambda: progress_bar.step(1))  # Incrémente la barre de progression
+
 
 
 def scroll_until_end(driver, scroll_pause_time):
@@ -276,6 +304,12 @@ search_bar.grid(row=0, column=0, padx=(5, 0), pady=15)
 
 search_button = ttk.Button(search_frame, text="Rechercher", command=search_all_sites)
 search_button.grid(row=0, column=1, padx=5)
+
+progress_frame = ttk.Frame(fen_main)
+progress_frame.pack(pady=(0, 5))  # Ajuste l'espace au besoin
+
+progress_bar = ttk.Progressbar(progress_frame, orient="horizontal", mode="determinate",bootstyle="info-striped", length=312)
+progress_bar.pack(padx=(5, 0))
 
 # Reducing the width of the second column (containing the search button)
 search_frame.columnconfigure(1, minsize=0, weight=0)
